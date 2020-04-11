d7083fb6c392873dcc6743db13cbda78006addeb
Dan <lyash.dv@gmail.com>
2020-04-11 18:25:54 +0300

    init

---
+++ src/main/java/net/dreamfteam/quiznet/QuizNetApplication.java
@@ -0,0 +1,13 @@
+package net.dreamfteam.quiznet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizNetApplication.class, args);
+	}
+
+}
