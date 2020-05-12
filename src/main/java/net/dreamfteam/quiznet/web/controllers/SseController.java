8b4be6854dbdd3337f8734ffe626b58db2848e08
Never04eK <heket844@gmail.com>
2020-05-12 18:13:57 +0300

    Update SseController.java

--- src/main/java/net/dreamfteam/quiznet/web/controllers/SseController.java
+++ src/main/java/net/dreamfteam/quiznet/web/controllers/SseController.java
@@ -1,21 +1,14 @@
 package net.dreamfteam.quiznet.web.controllers;
 
 import net.dreamfteam.quiznet.configs.Constants;
-import net.dreamfteam.quiznet.configs.security.AuthenticationFacade;
-import net.dreamfteam.quiznet.service.impl.GameConnectionsServiceImpl;
+import net.dreamfteam.quiznet.service.GameConnectionsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import reactor.core.publisher.Flux;
-
-import java.io.IOException;
-import java.time.Duration;
-import java.time.LocalTime;
 
 
 @Controller
@@ -23,23 +16,23 @@ import java.time.LocalTime;
 @RequestMapping(Constants.SSE_URLS)
 public class SseController {
 
-    private GameConnectionsServiceImpl IGameConnectionsServiceImpl;
+    private GameConnectionsService gameConnectionsService;
 
 
     @Autowired
-    public SseController(GameConnectionsServiceImpl IGameConnectionsServiceImpl) {
-        this.IGameConnectionsServiceImpl = IGameConnectionsServiceImpl;
+    public SseController(GameConnectionsService gameConnectionsService) {
+        this.gameConnectionsService = gameConnectionsService;
     }
 
     // handle normal "Async timeout", to avoid logging warn messages every 30s per client...
     @ExceptionHandler(value = AsyncRequestTimeoutException.class)
-    public String asyncTimeout(AsyncRequestTimeoutException e){
+    public String asyncTimeout(AsyncRequestTimeoutException e) {
         return null; // "SSE timeout..OK";
     }
 
     @GetMapping(path = "/stream/{key}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ServerSentEvent<String>> streamFlux(@PathVariable String key) {
-        return IGameConnectionsServiceImpl.subscribe(key);
+        return gameConnectionsService.subscribe(key);
     }
 
 }
