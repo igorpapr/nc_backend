6ffdf8c0c5dfe3db0be5621a39db8a6559d3d1dc
Never04eK <heket844@gmail.com>
2020-05-19 00:46:18 +0300

    Try fix heroku sse bug

--- src/main/java/net/dreamfteam/quiznet/configs/scheduler/UserScheduler.java
+++ src/main/java/net/dreamfteam/quiznet/configs/scheduler/UserScheduler.java
@@ -3,10 +3,13 @@ package net.dreamfteam.quiznet.configs.scheduler;
 import lombok.extern.slf4j.Slf4j;
 import net.dreamfteam.quiznet.data.dao.UserDao;
 import net.dreamfteam.quiznet.service.NotificationService;
+import net.dreamfteam.quiznet.service.SseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
 import static javax.management.timer.Timer.ONE_DAY;
+import static javax.management.timer.Timer.ONE_SECOND;
 
 @Slf4j
 @Component
@@ -14,11 +17,13 @@ public class UserScheduler {
 
     private final UserDao userDao;
     private final NotificationService notificationService;
+    private final SseService sseService;
 
     @Autowired
-    public UserScheduler(UserDao userDao, NotificationService notificationService) {
+    public UserScheduler(UserDao userDao, NotificationService notificationService, SseService sseService) {
         this.userDao = userDao;
         this.notificationService = notificationService;
+        this.sseService = sseService;
     }
 
     @Scheduled(fixedDelay = ONE_DAY)
@@ -27,9 +32,19 @@ public class UserScheduler {
     }
 
     @Scheduled(fixedDelay = ONE_DAY)
-    public void deleteHalfYearNotifications(){
+    public void deleteHalfYearNotifications() {
         notificationService.deleteHalfYear();
         log.info("Deleted half year notifications");
+    }
+
+    @Scheduled(fixedDelay = ONE_SECOND * 20)
+    public void sendSseSubscribersMessage() {
+        // for heroku https://devcenter.heroku.com/articles/error-codes#h15-idle-connection
+        for (String key : sseService.getSseMap()
+                                    .keySet()) {
+            sseService.send(key, "it works");
+        }
+        log.info("Send message to all sse subscribers");
     }
 
 }
