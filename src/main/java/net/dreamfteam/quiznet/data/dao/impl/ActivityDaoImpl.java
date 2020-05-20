046927771f3329f5b58f5a477da19957f4f33864
Ihor Paprotskyi <igorpapr@gmail.com>
2020-05-20 23:26:07 +0300

    Added activities filtering by user settings

--- src/main/java/net/dreamfteam/quiznet/data/dao/impl/ActivityDaoImpl.java
+++ src/main/java/net/dreamfteam/quiznet/data/dao/impl/ActivityDaoImpl.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -25,20 +26,29 @@ public class ActivityDaoImpl implements ActivityDao {
 	@Override
 	public List<FriendsActivity> getFriendsActivitiesList(String userId) {
 		try{
-			return jdbcTemplate.query("SELECT activity_id, content, datetime, ua.user_id, u.username, u.image AS image_content " +
-											"FROM user_activities ua INNER JOIN users u ON ua.user_id = u.user_id " +
-											"WHERE ua.user_id IN " +
-																"(SELECT f.friend_id AS id " +
-																"FROM friends f " +
-																"WHERE f.parent_id = uuid(?) " +
-																"AND f.accepted_datetime IS NOT NULL " +
-																"UNION " +
-																"SELECT f1.parent_id AS id " +
-																"FROM friends f1 " +
-																"WHERE f1.friend_id = uuid(?) " +
-																"AND f1.accepted_datetime IS NOT NULL) " +
-											"ORDER BY datetime DESC;",
-					new Object[]{userId, userId}, new ActivityMapper());
+			return jdbcTemplate
+					.query("SELECT activity_id, content, datetime, ua.user_id, u.username, u.image AS image_content " +
+								"FROM user_activities ua INNER JOIN users u ON ua.user_id = u.user_id " +
+								"INNER JOIN activity_types at1 ON ua.type_id = at1.type_id " +
+								"WHERE ua.user_id IN " + //selecting friends
+													"(SELECT f.friend_id AS id " +
+													"FROM friends f " +
+													"WHERE f.parent_id = uuid(?) " +
+													"AND f.accepted_datetime IS NOT NULL " +
+													"UNION " +
+													"SELECT f1.parent_id AS id " +
+													"FROM friends f1 " +
+													"WHERE f1.friend_id = uuid(?) " +
+													"AND f1.accepted_datetime IS NOT NULL) " +
+								//filtering by settings
+								"AND ua.type_id IN (SELECT activity_type_id " +
+													"FROM settings s INNER JOIN user_settings us " +
+													"ON s.setting_id = us.setting_id " +
+													"WHERE us.user_id = uuid(?) " +
+													"AND value = 'true' " +
+													"AND activity_type_id IS NOT NULL) " +
+								"ORDER BY datetime DESC;",
+					new Object[]{userId, userId, userId}, new ActivityMapper());
 		}catch (EmptyResultDataAccessException e){
 			return null;
 		}
