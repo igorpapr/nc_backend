2661ccf7f44374089c3e6d4c4118ab3306fbab1c
Dan <lyash.dv@gmail.com>
2020-05-20 12:08:49 +0300

    fix list of map

--- src/main/java/net/dreamfteam/quiznet/service/impl/GameSessionServiceImpl.java
+++ src/main/java/net/dreamfteam/quiznet/service/impl/GameSessionServiceImpl.java
@@ -1,20 +1,21 @@
 package net.dreamfteam.quiznet.service.impl;
 
-import net.dreamfteam.quiznet.configs.security.AuthenticationFacade;
 import net.dreamfteam.quiznet.data.dao.GameDao;
 import net.dreamfteam.quiznet.data.dao.GameSessionDao;
 import net.dreamfteam.quiznet.data.entities.ActivityType;
 import net.dreamfteam.quiznet.data.entities.GameSession;
-import net.dreamfteam.quiznet.exception.ValidationException;
-import net.dreamfteam.quiznet.service.*;
+import net.dreamfteam.quiznet.service.AchievementService;
+import net.dreamfteam.quiznet.service.ActivitiesService;
+import net.dreamfteam.quiznet.service.GameSessionService;
+import net.dreamfteam.quiznet.service.SseService;
 import net.dreamfteam.quiznet.web.dto.DtoActivity;
 import net.dreamfteam.quiznet.web.dto.DtoGameSession;
 import net.dreamfteam.quiznet.web.dto.DtoGameWinner;
+import net.dreamfteam.quiznet.web.dto.DtoPlayerSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class GameSessionServiceImpl implements GameSessionService {
@@ -22,21 +23,17 @@ public class GameSessionServiceImpl implements GameSessionService {
     private final GameSessionDao gameSessionDao;
     private final GameDao gameDao;
     private final SseService sseService;
-    private final GameService gameService;
-    private final AuthenticationFacade authenticationFacade;
     private final ActivitiesService activitiesService;
     private final AchievementService achievementService;
 
 
     @Autowired
-    public GameSessionServiceImpl(GameSessionDao gameSessionDao, SseService sseService, GameService gameService,
-                                  AuthenticationFacade authenticationFacade, ActivitiesService activitiesService,
+    public GameSessionServiceImpl(GameSessionDao gameSessionDao, SseService sseService,
+                                  ActivitiesService activitiesService,
                                   GameDao gameDao,
                                   AchievementService achievementService) {
         this.gameSessionDao = gameSessionDao;
         this.sseService = sseService;
-        this.gameService = gameService;
-        this.authenticationFacade = authenticationFacade;
         this.activitiesService = activitiesService;
         this.gameDao = gameDao;
         this.achievementService = achievementService;
@@ -64,7 +61,7 @@ public class GameSessionServiceImpl implements GameSessionService {
     }
 
     @Override
-    public List<Map<String, String>> getSessions(String gameId) {
+    public List<DtoPlayerSession> getSessions(String gameId) {
         return gameSessionDao.getSessions(gameId);
     }
 
@@ -85,26 +82,26 @@ public class GameSessionServiceImpl implements GameSessionService {
         return gameSessionDao.getGameId(sessionId);
     }
 
-    private void checkForGameOver(String gameId){
+    private void checkForGameOver(String gameId) {
         boolean isGameFinished = gameSessionDao.isGameFinished(gameId);
         if (isGameFinished) {
             int res = gameSessionDao.setWinnersForTheGame(gameId);
             sseService.send(gameId, "finished", gameId);
-            if(res > 0){//setting activities
+            if (res > 0) {//setting activities
                 List<DtoGameWinner> winners = gameDao.getWinnersOfTheGame(gameId);
-                for (DtoGameWinner winner : winners){
+                for (DtoGameWinner winner : winners) {
                     DtoActivity activity = DtoActivity.builder()
                             .userId(winner.getUserId())
                             .activityType(ActivityType.GAMEPLAY_RELATED)
-                            .content("Won the game while playing the quiz: \""+winner.getQuizTitle()+"\"")
+                            .content("Won the game while playing the quiz: \"" + winner.getQuizTitle() + "\"")
                             .build();
                     activitiesService.addActivityForUser(activity);
                 }
             }
             //checking achievements
-            List<Map<String, String>> sessionsMaps = getSessions(gameId);
-            for (Map<String,String> session : sessionsMaps) {
-                achievementService.checkAftergameAchievements(session.get("game_session_id"));
+            List<DtoPlayerSession> sessionsMaps = getSessions(gameId);
+            for (DtoPlayerSession session : sessionsMaps) {
+                achievementService.checkAftergameAchievements(session.getGame_session_id());
             }
 
             //sending message event to subscribers
