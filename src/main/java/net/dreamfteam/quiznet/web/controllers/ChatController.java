bccba648c02ae7123061d71d9ccc4951753196a6
Yevhen Khominich <zhekaukr328@gmail.com>
2020-05-28 15:15:12 +0300

    fix urls

--- src/main/java/net/dreamfteam/quiznet/web/controllers/ChatController.java
+++ src/main/java/net/dreamfteam/quiznet/web/controllers/ChatController.java
@@ -1,11 +1,9 @@
 package net.dreamfteam.quiznet.web.controllers;
 
-import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import net.dreamfteam.quiznet.configs.Constants;
 import net.dreamfteam.quiznet.configs.security.IAuthenticationFacade;
 import net.dreamfteam.quiznet.data.entities.Chat;
-import net.dreamfteam.quiznet.exception.ValidationException;
 import net.dreamfteam.quiznet.service.ChatService;
 import net.dreamfteam.quiznet.web.dto.DtoChatMessage;
 import net.dreamfteam.quiznet.web.dto.DtoChatWithParticipants;
@@ -13,9 +11,6 @@ import net.dreamfteam.quiznet.web.dto.DtoCreateGroupChat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -26,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
