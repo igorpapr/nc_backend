0242fd4b6acaaa9005fccc4a402b74f445e2ba08
Yevhen Khominich <zhekaukr328@gmail.com>
2020-05-28 15:17:34 +0300

    fix urls

--- src/main/java/net/dreamfteam/quiznet/web/controllers/WebSocketChatController.java
+++ src/main/java/net/dreamfteam/quiznet/web/controllers/WebSocketChatController.java
@@ -40,7 +40,7 @@ public class WebSocketChatController {
 
 		chatService.saveMessage(chatId, dtoChatMessage);
 
-		messagingTemplate.convertAndSend("topic/message/" + chatId, new Gson().toJson(dtoChatMessage));
+		messagingTemplate.convertAndSend("topic/chat/" + chatId, new Gson().toJson(dtoChatMessage));
 	}
 
 }
